3a44e2dba1b6ca4132f2058a97aad94d6f5d3156
Alex Li <621alexl@gmail.com>
2019-10-29 16:21:47 -0400

    Updated python script to return dates of last use

--- weeklyUserGetter.py
+++ weeklyUserGetter.py
@@ -10,10 +10,11 @@ userList = []
 indexList = []
 userMatrix = [[]]
 emailList = []
-dayList = []
+dateList = []
 wabuJSON = None
 
-with open('WaBu Data.json', errors = "ignore") as json_file:
+json_file_name = input("Input JSON File name in same folder as this file: ")
+with open(json_file_name, errors = "ignore") as json_file:
     wabuJSON = json.load(json_file)
     
 for i in wabuJSON["USERS"].keys():
@@ -25,17 +26,20 @@ userList = userList[1:]
 
 userJSON = wabuJSON["USERS"]
 for i in userList:
-
-    secondsElapsed = CURRENT_UNIX_TIME - wabuJSON["USERS"][i]["TIME_MILLIS"]
+    UNIXSeconds = wabuJSON["USERS"][i]["TIME_MILLIS"]
+    secondsElapsed = CURRENT_UNIX_TIME - UNIXSeconds
     if (secondsElapsed <= WEEK_IN_SECONDS):
         indexList.append(userJSON[i]["USER_NAME"])
         emailList.append(userJSON[i]["EMAIL"])
-        daysSinceUse = secondsElapsed / 60 / 60 / 24
-        dayList.append(daysSinceUse)
+        date = str(datetime.datetime.utcfromtimestamp(UNIXSeconds).strftime("%Y-%m-%d %H:%M:%S"))
+        print(type(date))
+        #daysSinceUse = secondsElapsed / 60 / 60 / 24
+        dateList.append(date)
         
 
 
-dataDict = {"email": emailList, "days_since_use": dayList}
+dataDict = {"email": emailList, "date_of_use": dateList}
 df = pd.DataFrame(data=dataDict, index = indexList)
+df = df.sort_values("date_of_use")
 df.to_csv('users.csv')
 # 1572255490.7196
